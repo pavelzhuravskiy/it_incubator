0ff2b9cd1c04b1654883e98bdf003066ee65911e
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-01-16 16:07:47 +0200

    Path of the Samurai videos (008)

---
+++ Path_of_the_Samurai/express/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express"
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const a = 4;
+  if (a > 5) {
+    res.send("OK!");
+  } else {
+    res.send("Hello World!");
+  }
+});
+
+app.get("/samurais", (req, res) => {
+  res.send("Hello samurai, Zhur");
+});
+
+app.post("/samurais", (req, res) => {
+  res.send("We have created samurai ");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
