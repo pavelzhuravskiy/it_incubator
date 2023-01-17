328119e9880764e2fd271ec968cf9f0868aab7ec
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-01-17 15:40:16 +0200

    Path of the Samurai videos (009)

--- Path_of_the_Samurai/express/src/index.ts
+++ Path_of_the_Samurai/express/src/index.ts
@@ -1,22 +1,89 @@
-import express from "express"
+import express from "express";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  const a = 4;
-  if (a > 5) {
-    res.send("OK!");
-  } else {
-    res.send("Hello World!");
+const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+};
+
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+
+const db = {
+  courses: [
+    {
+      id: 1,
+      title: "front-end",
+    },
+    {
+      id: 2,
+      title: "backend",
+    },
+    {
+      id: 3,
+      title: "automatation qa",
+    },
+    {
+      id: 4,
+      title: "devops",
+    },
+  ],
+};
+
+app.get("/courses", (req, res) => {
+  let foundCourses = db.courses;
+  if (req.query.title) {
+    foundCourses = foundCourses.filter(
+      (c) => c.title.indexOf(req.query.title as string) > -1
+    );
   }
+  res.json(foundCourses);
 });
-
-app.get("/samurais", (req, res) => {
-  res.send("Hello samurai, Zhur");
+app.get("/courses/:id", (req, res) => {
+  const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+  if (!foundCourse) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+  res.json(foundCourse);
 });
+app.post("/courses", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+    return;
+  }
 
-app.post("/samurais", (req, res) => {
-  res.send("We have created samurai ");
+  const createdCourse = {
+    id: +new Date(),
+    title: req.body.title,
+  };
+  db.courses.push(createdCourse);
+  res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+});
+app.delete("/courses/:id", (req, res) => {
+  db.courses = db.courses.filter((c) => c.id !== +req.params.id);
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+app.put("/courses/:id", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+  if (!foundCourse) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  foundCourse.title = req.body.title;
+
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
 
 app.listen(port, () => {
