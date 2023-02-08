c4be49cd84acc144dc391c2f1778d94bad958f05
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-08 17:51:47 +0200

    Added tests guide

--- _monday_/video_content/lesson_02/src/index.ts
+++ _monday_/video_content/lesson_02/src/index.ts
@@ -1,14 +1,53 @@
-import express, {Request, Response} from "express"
+import express, { NextFunction, Request, Response } from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+const blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  req.blabla = "hello";
+  next();
+};
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+const authGuardMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.query.token === "123") {
+    next();
+  } else {
+    res.send(401);
+  }
+};
+
+let requestCounter = 0;
+
+const requestCounterMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  requestCounter++;
+  next();
+};
+
+app.use(blablaMiddleware);
+app.use(requestCounterMiddleware)
+// app.use(authGuardMiddleware);
+
+app.get("/products", (req: Request, res: Response) => {
+  // @ts-ignore
+  const blabla = req.blabla;
+  res.send({ value: `${blabla}!!! + ${requestCounter}` });
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  // @ts-ignore
+  const blabla = req.blabla;
+  res.send({ value: `${blabla}!!! + ${requestCounter}` });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
